e5da677f01681ae585dae5aabded9a7a26e1113f
Juan Cruz Marquez <marquezjuanchy@hotmail.com>
2021-08-14 00:19:02 -0300

    Commit 1

---
+++ README.md
@@ -0,0 +1,2 @@
+### Aplicacion de consola creada con NodeJs para listar tareas
+### Con funciones como agregar, eliminar, editar tareas (Cumplidas o pendientes en determinada fecha)
---
+++ models/tareas.js
@@ -0,0 +1,112 @@
+
+
+const Tarea = require('./tarea');
+
+
+class Tareas {
+
+    _listado = {};
+
+
+    get listadoArr(){
+
+        const listado = [];
+
+        Object.keys(this._listado).forEach( key => {
+            const tarea = this._listado[key];
+            listado.push(tarea);
+        });
+
+        return listado;
+    }
+    
+    constructor(){
+        this._listado = {};
+    }
+
+    borrarTarea(id=''){
+        if (this._listado[id]){
+            delete this._listado[id];
+        }
+    }
+
+    cargarTareasFromArray(tareas = []){
+        
+        tareas.forEach( tarea => {
+            this._listado[tarea.id] = tarea;
+        })
+    }
+
+    crearTarea(desc = ''){
+        const tarea = new Tarea(desc);
+        this._listado[tarea.id] = tarea;
+    }
+
+    listadoCompleto (){
+        console.log('');
+        this.listadoArr.forEach((tarea,i)=>{
+            const idx = `${i + 1}.`.green
+            const {desc, completadoEn} = tarea;
+            const estado = (completadoEn)
+                            ? 'Completada'.green
+                            : 'Pendiente'.red
+            console.log(`${idx} ${desc} :: ${estado}`);
+
+        });
+    }
+
+    listarPendientesCompletadas(completadas = true){
+
+        console.log('');
+        let contador = 0;
+        this.listadoArr.forEach(tarea => {
+            const {desc, completadoEn} = tarea;
+            const estado = (completadoEn)
+                            ? 'Completada'.green
+                            : 'Pendiente'.red
+            
+            if(completadas){
+                if (completadoEn){
+                    contador += 1 ;
+                    console.log(`${ (contador + '.').green} ${desc} :: ${estado} en: ${completadoEn.green}`);
+
+                }
+            }else {
+                if(!completadoEn){
+                    contador += 1 ;
+                    console.log(`${(contador + '.').red} ${desc} :: ${estado}`);
+
+                }
+
+            }
+
+        });
+
+    }
+
+    toggleCompletadas(ids=[]){
+
+        ids.forEach(id =>{
+            const tarea = this._listado[id];
+            if (!tarea.completadoEn){
+                const date = new Date()
+                const dia = date.getDate().toString()
+                const anio = date.getFullYear().toString()
+                const mes = date.getMonth().toString()
+                const hora = date.getHours().toString()
+                const min = date.getMinutes().toString()
+                const sec = date.getSeconds().toString()
+                tarea.completadoEn = `${anio.green}-${mes.green}-${dia.green}  ${hora.blue}:${min.blue}:${sec.blue}`
+            }
+        })
+
+        this.listadoArr.forEach( tarea =>{
+            if (!ids.includes(tarea.id)){
+                this._listado[tarea.id].completadoEn = null;
+            }
+        })
+
+    }
+}
+
+module.exports = Tareas; 
